62bbbf17400fce251ac7e47fb6baeeca9b9ae31c
AlphaMale <hallowenbirk@gmail.com>
2025-04-03 13:11:22 +0200

    Finished a good first version for MQTT for all the merds.

---
+++ Elsys FiskeApp/Model/BrokerClient.cs
@@ -0,0 +1,108 @@
+﻿using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using MQTTnet.Formatter;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using MQTTnet.Server.EnhancedAuthentication;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.Net;
+using System.Text.Json;
+
+namespace MQTTnet;
+
+public class BrokerClient
+{
+
+    public IMqttClient brokerClient;
+    public string brokerName;
+    string ip;
+    int port;
+    public Queue<Elsys_FiskeApp.updateData> inputData; // Contains all input for the given brokerclient.
+
+
+    ~BrokerClient()
+    {
+        DisconnectFromBroker();
+    }
+
+    public void setBrokerSetttings(Elsys_FiskeApp.ClientConnectionSettings settings)
+    {
+        brokerName = settings.merdName;
+        ip = settings.ip;
+        port = settings.port;
+    }
+    public async Task ConnectToBroker(CancellationToken token) // Returns the result of the connection
+    {
+
+        MqttClientFactory mqttFactory = new MqttClientFactory();
+        brokerClient = mqttFactory.CreateMqttClient();
+        var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(ip, port).Build();
+        MqttClientConnectResult result = await brokerClient.ConnectAsync(mqttClientOptions, token);
+        if (result.ResultCode == MqttClientConnectResultCode.Success)
+        {
+            brokerClient.ApplicationMessageReceivedAsync -= HandleMessageReceived;
+            brokerClient.ApplicationMessageReceivedAsync += HandleMessageReceived;
+        }
+
+    }
+
+    public async Task DisconnectFromBroker()
+    {
+        await brokerClient.DisconnectAsync(new MqttClientDisconnectOptionsBuilder()
+        .WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
+        .Build());
+        brokerClient.Dispose();
+
+    }
+
+    public async Task Subscribe(string topic, MqttQualityOfServiceLevel qos, CancellationToken token)
+    {
+        var subOptions = new MqttClientSubscribeOptionsBuilder()
+            .WithTopicFilter(f => f.WithTopic(topic).WithQualityOfServiceLevel(qos)) 
+            .Build();
+
+        await brokerClient.SubscribeAsync(subOptions, token);
+        Console.WriteLine($"MQTT client subscribed to topic '{topic}' with QoS {qos}.");
+    }
+
+    public async Task UnSubscribe(string topic, CancellationToken token)
+    {
+        MqttClientUnsubscribeOptions unSubOptions = new MqttClientUnsubscribeOptionsBuilder().
+        WithTopicFilter(topic).Build();
+
+        await brokerClient.UnsubscribeAsync(unSubOptions, token);
+        Console.WriteLine($"MQTT client unsubscribed from topic '{topic}'.");
+    }
+    public async Task PublishMessage(string topic, string Message, MqttQualityOfServiceLevel QOSL)
+    {
+        var applicationMessage = new MqttApplicationMessageBuilder()
+            .WithTopic(topic).WithPayload(Message).WithQualityOfServiceLevel(QOSL)
+            .Build();
+
+        await brokerClient.PublishAsync(applicationMessage);
+    }
+
+    public async Task PublishFloatArray(string topic, float[] values, MqttQualityOfServiceLevel QOSL)
+    {
+        string payload = JsonSerializer.Serialize(values);
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic(topic).WithPayload(payload).WithQualityOfServiceLevel(QOSL)
+            .Build();
+        await brokerClient.PublishAsync(message);
+    }
+
+
+    private Task HandleMessageReceived(MqttApplicationMessageReceivedEventArgs e)
+    {
+        var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+        Console.WriteLine($"Received message on topic '{e.ApplicationMessage.Topic}': {payload}");
+        return Task.CompletedTask;
+    }
+
+
+}
+
+
---
+++ Elsys FiskeApp/Model/BrokerClientsHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using System.Text.Json;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace Elsys_FiskeApp
+{
+    
+    public class BrokerClientsHandler // handles all the clients
+    {
+        List<BrokerClient> BrokerClients;
+        public static BrokerClientsHandler Instance;
+        Dictionary<string, Queue<updateData>> inputsDataQueues; // input : name of merd. Output : the input data given from the hydrophone.
+
+
+        public ObservableCollection<ClientConnectionSettings> ClientsConnectionSettings;
+        public BrokerClientsHandler()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                BrokerClients = new List<BrokerClient>();
+                inputsDataQueues = new Dictionary<string, Queue<updateData>>();
+                LoadClientsConnectionSettings();
+                ClientsConnectionSettings.CollectionChanged += (sender, e) => SaveClientsConnectionSettings();
+
+                foreach (var clientConnectionSettings in ClientsConnectionSettings) // Creates and setups brokers and ties input data to the dict.
+                {
+                    var newClient = new BrokerClient();
+                    newClient.setBrokerSetttings(clientConnectionSettings);
+                    BrokerClients.Add(newClient);
+                    inputsDataQueues.Add(clientConnectionSettings.merdName, newClient.inputData);
+                }
+
+                ConnectAllBrokerClients();
+
+            }
+
+        }
+
+        public async Task ConnectAllBrokerClients()
+        {
+            List<Task> tasks = new List<Task>();
+            foreach (var client in BrokerClients)
+            {
+                tasks.Add(client.ConnectToBroker(CancellationToken.None));
+            }
+            await Task.WhenAll(tasks);
+        }
+            
+        void LoadClientsConnectionSettings()
+        {
+            string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName; // Appdomain.Currentdomain.Basedirectory gives the basedirectory inside the bin. Since the bin is 4 steps inside the main directory, by parenting 4 times, we can access the original .json file.
+            string filePath = Path.Combine(projectDirectory, "Model", "ClientsConnectionSettings.json");
+
+
+            if (File.Exists(filePath))
+            {
+                string input = File.ReadAllText(filePath);
+                ClientsConnectionSettings = JsonSerializer.Deserialize<ObservableCollection<ClientConnectionSettings>>(input) ?? new ObservableCollection<ClientConnectionSettings>();
+            }
+            else
+            {
+                ClientsConnectionSettings = new ObservableCollection<ClientConnectionSettings>(); // Just make one if the filepath doesnt exist. We will later save it so 
+                //the next time it tries to access filePath, filePath exists. 
+            }
+        }
+        public void SaveClientsConnectionSettings()
+        {
+            string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string filePath = Path.Combine(projectDirectory, "Model", "ClientsConnectionSettings.json");
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            string serializedJson = JsonSerializer.Serialize(ClientsConnectionSettings, options);
+            File.WriteAllText(filePath, serializedJson);
+        }
+
+
+
+
+    }
+}
+
+
+
